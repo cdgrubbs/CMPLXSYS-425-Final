38a4b2f0034bde9cd5ab5374e8dd780caa8763c1
cdgrubbs <cdgrubbs@umich.edu>
2019-04-30 12:13:58 -0400

    Add files via upload

---
+++ FinalModel.py
@@ -0,0 +1,38 @@
+inp = input("input a genre: ")
+class Song:
+    def __init__(self, fitness, array, wav):
+        self.fitness = fitness
+        self.array = array
+        self.wav = wav
+
+    def set_fitness():
+        # TODO:
+        return
+    
+    def mutate():
+        # TODO:
+        return
+
+    def __repr__(self):
+        return "Item array: {0}, fitness: {1}".format(self.array, self.fitness)
+
+def recombination():
+    # TODO:
+    return
+
+song_list = []
+data_list = []
+
+for item in song_list:
+    data_list.append(Song(get_genre_class(item),item))
+
+population = songs[]
+for generation in range(num_generations):
+    for song in population:
+        song.set_fitness()
+    # Create X many new songs by re arranging beats
+
+    # Mutate new songs
+    for song in population:
+
+    # Keep it goin
