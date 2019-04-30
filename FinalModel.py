5cdd997107b41228d57fa12a6a6a1f0882061eda
cdgrubbs <cdgrubbs@umich.edu>
2019-04-30 12:47:27 -0400

    Update FinalModel.py

--- FinalModel.py
+++ FinalModel.py
@@ -15,9 +15,38 @@ class Song:
     def __repr__(self):
         return "Item array: {0}, fitness: {1}".format(self.array, self.fitness)
 
-def recombination(Song a, Song b):
-    # TODO:
-    return
+def recombination(a, b):
+    a_len = len(a)
+    b_len = len(b)
+    
+    a_to_recomb = a_len / 10.0
+    b_to_recomb = b_len / 10.0
+    
+    pool[a_to_recomb + b_to_recomb]
+    
+    a_chosen[a_to_recomb]
+    b_chosen[b_to_recomb]
+    
+    while len(a_chosen) != a_to_recomb:
+        index = random.randint(0, a_len)
+        if (a_chosen.count(index) == 0):
+            a_chosen.append(index)
+            pool.append(a[index])
+
+    while len(b_chosen) != b_to_recomb:
+        index = random.randint(0, a_len)
+        if (b_chosen.count(index) == 0):
+            b_chosen.append(index)
+            pool.append(b[index])
+
+    for i in range(len(a_chosen)):
+        index = random.randint(0, len(pool))
+        a[a_chosen[i]] = pool[index]
+
+    for i in range(len(b_chosen)):
+        index = random.randint(0, len(pool))
+        b[b_chosen[i]] = pool[index]
+    return {a, b}
 
 song_list = []
 data_list = []
